a8c48c984252f093dd3147dbeb0c3ed080e63b08
Emmanuel <emmlop243@gmail.com>
2021-10-09 18:27:23 -0500

    Maquetación de Pokemon Picture

---
+++ src/components/PokemonOptions.vue
@@ -0,0 +1,11 @@
+<template>
+  <h3>Pokemon Options</h3>
+</template>
+
+<script>
+export default {
+  name: 'pokemon-options'
+};
+</script>
+
+<style></style>
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,9 +1,15 @@
 <template>
   <h1>Who is this pokemon?</h1>
+  <pokemon-picture />
+  <pokemon-options />
 </template>
 
 <script>
+import PokemonOptions from '@/components/PokemonOptions.vue';
+import PokemonPicture from '@/components/PokemonPicture.vue';
+
 export default {
+  components: { PokemonPicture, PokemonOptions },
   name: 'pokemon-page'
 };
 </script>
