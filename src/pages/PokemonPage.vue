f574866a0f9a451d37ebccf52e0c20b2f736afd4
Emmanuel <emmlop243@gmail.com>
2021-10-09 19:48:49 -0500

    Game Basic Funcionality Done: It selects a random pokemon from the four pokemons preselected, and we can guess it by click one the four shown options

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,7 +1,15 @@
 <template>
-  <h1>Who is this pokemon?</h1>
-  <pokemon-picture :pokemonId="1" :show-pokemon="true" />
-  <pokemon-options :pokemons="pokemonArr" />
+  <h1 v-if="!pokemon">Espere por favor...</h1>
+  <div v-else>
+    <h1>Who is this pokemon?</h1>
+    <pokemon-picture :pokemon-id="pokemon.id" :show-pokemon="showPokemon" />
+    <pokemon-options :pokemons="pokemonArr" @selection="checkAnswer" />
+
+    <template v-if="showAnswer">
+      <h2>{{ message }}</h2>
+      <button @click="newGame">Nuevo juego</button>
+    </template>
+  </div>
 </template>
 
 <script>
@@ -14,12 +22,30 @@ export default {
   components: { PokemonPicture, PokemonOptions },
   data() {
     return {
-      pokemonArr: []
+      pokemonArr: [],
+      pokemon: null,
+      showPokemon: false,
+      showAnswer: false,
+      message: ''
     };
   },
   methods: {
     async mixPokemonArr() {
       this.pokemonArr = await getPokemonOptions();
+      const rdnInt = Math.floor(Math.random() * 4);
+      this.pokemon = this.pokemonArr[rdnInt];
+    },
+    checkAnswer(selectedId) {
+      this.showPokemon = true;
+      this.showAnswer = true;
+      if (selectedId === this.pokemon.id) this.message = `Correcto, ${this.pokemon.name}`;
+      else this.message = `Oops, era ${this.pokemon.name}`;
+    },
+    async newGame() {
+      this.showPokemon = false;
+      this.showAnswer = false;
+      this.pokemon = null;
+      await this.mixPokemonArr();
     }
   },
   mounted() {
